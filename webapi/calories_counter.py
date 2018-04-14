98b7a2207a2405c427c983f6b4c22758cad19ad0
Szymon <sz.rzad@gmail.com>
2018-04-14 15:36:59 +0200

    Statyczne liczenie kalorii i obuga JSON na żadaniach POST

--- webapi/calories_counter.py
+++ webapi/calories_counter.py
@@ -1,5 +1,66 @@
-def calories_burned(uphill, downhill, length, activity, weight, age, ):
-    calories = 0
+# Wartości wzięte z https://sites.google.com/site/compendiumofphysicalactivities/Activity-Categories
+MET_WALKING_UPHILL_1_5 = 5.3
+MET_WALKING_UPHILL_6_15 = 8.0
+MET_WALKING_DOWNHILL = 3.3
+MET_WALKING_LEVEL = 4.3
 
+MET_RUNNING_UPHILL_1_5 = 1.0
+MET_RUNNING_UPHILL_6_15 = 3.0
+MET_RUNNING_DOWNHILL = -1.2
+MET_RUNNING_8KMH = 8.3
+MET_RUNNING_10KMH = 9.1
+
+UPHILL_TIME_OVER_DOWNHILL_TIME = 0.5
+
+
+def walking_MET(up_grade):
+    down_met = MET_WALKING_DOWNHILL
+
+    # Ustawienie wspołczynnika MET w zależności od ostrości powierzchni
+    if up_grade < 6.0:
+        up_met = MET_WALKING_UPHILL_1_5
+    else:
+        up_met = MET_WALKING_UPHILL_6_15
+
+    return down_met, up_met
+
+
+def running_MET(speed, up_grade):
+    if speed < 9.0:
+        down_met = MET_RUNNING_8KMH
+        up_met = MET_RUNNING_8KMH
+    else:
+        down_met = MET_RUNNING_10KMH
+        up_met = MET_RUNNING_10KMH
+
+    # Ustawienie wspołczynnika MET w zależności od ostrości powierzchni
+    if 0.0 < up_grade < 6.0:
+        up_met += MET_RUNNING_UPHILL_1_5
+    elif up_grade >= 6.0:
+        up_met += MET_RUNNING_UPHILL_6_15
+
+    return down_met, up_met
+
+
+def uphill_grade(length, uphill):
+    return 100 * uphill / length
+
+
+def calories_burned(uphill_in_km, length_in_km, time_in_h, activity, weight_in_kg):
+    calories, down_met, up_met = 0, 0, 0
+    up_grade = uphill_grade(length_in_km, uphill_in_km)
+    speed = length_in_km / time_in_h
+
+    if activity == "walking":
+        down_met, up_met = walking_MET(up_grade)
+    elif activity == "running":
+        down_met, up_met = running_MET(speed, up_grade)
+
+    calories += up_met * UPHILL_TIME_OVER_DOWNHILL_TIME * weight_in_kg * time_in_h
+
+    calories += down_met * (1 - UPHILL_TIME_OVER_DOWNHILL_TIME) * weight_in_kg * time_in_h
 
     return calories
+
+
+print(calories_burned(0.0, 3.5, 0.5, "running", 80))
--- webapi/server.py
+++ webapi/server.py
@@ -1,7 +1,7 @@
 from http.server import BaseHTTPRequestHandler, HTTPServer
+import json
 
-
-class S(BaseHTTPRequestHandler):
+class Server(BaseHTTPRequestHandler):
     def _set_headers(self):
         self.send_response(200)
         self.send_header('Content-type', 'text/html')
@@ -15,25 +15,24 @@ class S(BaseHTTPRequestHandler):
         self._set_headers()
 
     def do_POST(self):
-        # Doesn't do anything with posted data
         content_length = int(self.headers['Content-Length'])  # <--- Gets the size of data
         post_data = self.rfile.read(content_length)  # <--- Gets the data itself
+        response = "Zwrotka"
+        try:
+            json_data = json.loads(post_data.decode())
+            print(json.dumps(json_data))
+        except json.decoder.JSONDecodeError:
+            response = "Blad konwersji JSON"
+
         self._set_headers()
-        self.wfile.write(post_data)
+        self.wfile.write(str.encode(response + '\n'))
 
 
-def run(server_class=HTTPServer, handler_class=S, port=80):
+def run(server_class=HTTPServer, handler_class=Server, port=80):
     server_address = ('', port)
     httpd = server_class(server_address, handler_class)
-    print
-    'Starting httpd...'
     httpd.serve_forever()
 
 
 if __name__ == "__main__":
-    from sys import argv
-
-    if len(argv) == 2:
-        run(port=8080)
-    else:
-        run()
+    run(port=8080)
